bbd5ac5a789c40cd140a078f86c0a9e06c549190
jessechen0319 <xiang19890319@gmail.com>
2016-11-29 16:53:17 +0800

    optimization

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,7 @@ app.controller('index', function ($scope, $http, $rootScope) {
 	$scope.stockProcessPercentage = 0;
 	$scope.trackLastFiveDays = [];
 	$rootScope.errors = [];
+	$scope.disableFetchStockFlag = false;
 
 	$rootScope.clearThisError = function(message){
 
@@ -20,12 +21,40 @@ app.controller('index', function ($scope, $http, $rootScope) {
 	};
 
 	$scope.fetchStockDetail = function(){
+
+		var todayFetchStatus;
+		$scope.dateValidations.forEach(function(item){
+			if (item.date == dateFormate) {
+				todayFetchStatus = item;
+			}
+		});
+
+		if(todayFetchStatus && todayFetchStatus.valid == 'validate-success'){
+			$rootScope.errors.push('Today data has already been fetched, please fetch data tomorrow');
+			return;
+		}
+
+		if(Number($scope.stockProcessPercentage) != 0 && Number($scope.stockProcessPercentage) != 100){
+			$rootScope.errors.push('Fetching, please do not run duplicated');
+			return;
+		}
+
+		$scope.disableFetchStockFlag = true;
+
 		$http({
 		  method: 'GET',
 		  url: '/storeStockToDB'
 		}).then(function successCallback(response) {
 			$rootScope.errors.push('The fetch is started');
+			$http({
+			  method: 'GET',
+			  url: '/getDetailProcessRate'
+			}).then(function successCallback(response) {
+				$scope.disableFetchStockFlag = false;
+			}, function errorCallback(response) {
+			});
 		}, function errorCallback(response) {
+			$scope.disableFetchStockFlag = false;
 			$rootScope.errors.push('The fetch is failed');
 		});
 	};
@@ -45,6 +74,14 @@ app.controller('index', function ($scope, $http, $rootScope) {
 		  url: '/getDetailProcessRate'
 		}).then(function successCallback(response) {
 			$scope.stockProcessPercentage = Number(response.data.processRate);
+			if(response.data.processRate == 100) {
+				//when it finished, refresh the calendar
+				$http.post('/checkFetchDates', { 'dates': $scope.trackLastFiveDays }).then(function successCallback(response) {
+					console.log(response.data);
+					$scope.dateValidations = response.data;
+				}, function errorCallback(response) {
+				});
+			}
 		}, function errorCallback(response) {
 		});
 	}, 2000);
