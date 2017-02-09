4520472014594ee2f62f74df09d786e92c5a8032
jesse <xiang19890319@gmail.com>
2017-02-09 13:23:57 +0800

    db

--- service/StockDetailFetchService.js
+++ service/StockDetailFetchService.js
@@ -79,7 +79,14 @@ var stockDetailService = (function(){
 					stockObject.amountMoney = Number(stockObject.amountMoney.toFixed(0));
 					stockObject.lastDayPrice = 0;
 
-					tempJson.push(stockObject);
+					MySqlService.query('insert into t_stock_detail (stock_code, begin_price, last_day_price, price, top_price, low_price, amount_stock, amount_money, date) values (?, ?,?,?,?,?,?,?,?)', [stockObject.stockCode, Number(stockObject.beginPrice), Number(stockObject.lastDayPrice), Number(stockObject.price), Number(stockObject.topPrice), Number(stockObject.lowPrice), Number(stockObject.amountStock), Number(stockObject.amountMoney), stockObject.date], function(err, result) {
+					  if (err){
+					  	logger.error('insert into DB error:');
+					  	logger.error(err);
+					  } else {
+					  	logger.info(`successfully insert->${code} at ${stockObject.date}`)
+					  }
+					});
 
 					if(index == data[0]['hq'].length-1){
 						logger.info(`init fetch finished for ${code}, tempJson file size is ${tempJson.length}`);
